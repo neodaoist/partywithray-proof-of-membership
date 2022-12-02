4670d7a1dbc071660ee901f818edb1a2c89838f7
Peter Laird <plaird@planview.com>
2022-12-02 09:14:38 -0700

    Deploy TheLow contract through cucumber framework

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,13 +1,24 @@
 mod solidity_bdd;
 use std::{env};
-use crate::solidity_bdd::TestConfig;
+use std::borrow::Borrow;
+use async_std::task;
+use crate::solidity_bdd::{SCWorld, TestConfig};
+use cucumber::{gherkin::Step, given, when, then, World};
+use ethers::prelude::k256;
+use ethers_contract::{abigen, Contract};
+use ethers_middleware::SignerMiddleware;
+use ethers_providers::{Http, Provider};
+use ethers_signers::Wallet;
 
 const FORK_CHAIN_ID: u64 = 31337_u64;
 
+// Generate bindings for contracts
+abigen!(TheLow, "out/TheLow.sol/TheLow.json");
 
+
+// Test configuration and runner
 
 #[tokio::main]
-// Test runner
 async fn main()
 {
     // Generate bindings for the 3rd party contracts we'll need
@@ -21,4 +32,13 @@ async fn main()
         //.thirdPartyContracts([])  // FIXME: Add VRF here if we end up using it
         ;
     solidity_bdd::run(config).await
+}
+
+// Stepdefs
+
+#[given("the Partywithray Proof of Membership NFT contract is deployed")]
+fn deploy_party_with_ray(world: &mut SCWorld) {
+    println!("Deploying the low");
+    let thelow_contract = task::block_on(solidity_bdd::the_low::TheLow::deploy(world.client(), ()).expect("Failed to deploy").send()).expect("Failed to send");
+    world.thelow_contract = Some(thelow_contract);
 }
--- tests/solidity_bdd.rs
+++ tests/solidity_bdd.rs
@@ -1,22 +1,58 @@
 use std::{env, fs};
 use std::collections::HashMap;
 use std::fmt::Formatter;
+use std::borrow::Borrow;
 use ethers::utils::{Anvil, AnvilInstance};
 use ethers_providers::{Provider, Http};
 use ethers::prelude::*;
 use ethers_contract::Contract;
 use std::time::Duration;
 use std::sync::Arc;
+use async_std::task;
 use cucumber::{ World, writer, WriterExt };
 use ethers_core::k256;
 use futures::FutureExt;
 
+abigen!(TheLow, "out/TheLow.sol/TheLow.json");
+
 #[derive(cucumber::World, Default, Debug)]
 pub struct SCWorld {
     config: TestConfig,
-    contracts: Vec<Contract<SignerMiddleware<Provider<Http>, Wallet<k256::ecdsa::SigningKey>>>>,
+    contracts: HashMap<String, ethers::contract::Contract<SignerMiddleware<Provider<Http>, Wallet<k256::ecdsa::SigningKey>>>>,
     addresses: HashMap<String, Address>,
+    address_count: usize,  // TODO: Refactor into a separate address book struct
     anvil: Option<AnvilConnection>,
+    pub thelow_contract: Option<the_low::TheLow<SignerMiddleware<Provider<Http>, Wallet<k256::ecdsa::SigningKey>>>> // FIXME: Find a generic way to do this
+}
+
+
+impl SCWorld {
+    pub fn deployer_address(&mut self) -> &Address {
+        self.get_address("deployer".to_string())
+    }
+    pub fn get_address(&mut self, name: String) -> &Address {
+        if ! self.addresses.contains_key(&name) {
+            let instance = self.anvil.as_ref().expect("Anvil must be initialized before client is called").anvil.borrow();
+            let wallet: LocalWallet = instance.keys()[self.address_count].clone().into();
+            self.address_count += 1;
+            self.addresses.insert(name.clone(), wallet.address());
+        }
+
+        self.addresses.get(&name).expect("Address not found by name after insert")
+
+    }
+    pub fn client(&self) -> Arc<SignerMiddleware<ethers_providers::Provider<Http>, Wallet<ethers::core::k256::ecdsa::SigningKey>>> {
+        let instance = self.anvil.as_ref().expect("Anvil must be initialized before client is called").anvil.borrow();
+        let provider = Provider::<Http>::try_from(instance.endpoint()).expect("Failed to connect to Anvil").interval(Duration::from_millis(10u64));
+        let wallet: LocalWallet = instance.keys()[0].clone().into();
+        let client = Arc::new(SignerMiddleware::new(provider, wallet.with_chain_id(instance.chain_id())));
+        client
+    }
+    pub fn add_contract(&mut self, contract: ethers::contract::Contract<SignerMiddleware<Provider<Http>, Wallet<k256::ecdsa::SigningKey>>>, name: String) {
+        self.contracts.insert(name, contract);
+    }
+
+
 }
 
 #[derive(Default, Debug)]
@@ -83,23 +119,26 @@ async fn start_anvil(fork_url: String, chain_id: u64, world: &mut SCWorld) {
 
 // impl ContractTestHelper for TestConfig {
 pub(crate) async fn run(config: TestConfig) -> () {
-    let file = fs::File::create(dbg!(format!("{}/junit.xml", env!("OUT_DIR")))).expect("File should be found");
+    //let file = fs::File::create(dbg!(format!("{}/junit.xml", env!("OUT_DIR")))).expect("File should be found");
     let fork_url = config.fork_endpoint.unwrap_or("localhost:8545".to_string());
     let chain_id = config.fork_chain_id.unwrap_or(31337_u64);
-        //let world =
-        SCWorld::cucumber()
-            // Start a fresh anvil before each scenario
-            .before(move |_feature, _rule, _scenario, world| {
-                    start_anvil(fork_url.clone(), chain_id.clone(), world).boxed_local()
-            })
-            .with_writer(
-                // Output to both console and JUnit XML
-                // NOTE: `Writer`s pipeline is constructed in a reversed order.
-                writer::Basic::stdout() // And output to STDOUT.
-                    .summarized()       // Simultaneously, add execution summary.
-                    .tee::<SCWorld, _>(writer::JUnit::for_tee(file, 0)) // Then, output to XML file.
-                    .normalized()       // First, normalize events order.
-            )
-            .run("tests/features")
-            .await;
-    }
+
+    SCWorld::cucumber()
+        // Start a fresh anvil before each scenario
+        .before(move |_feature, _rule, _scenario, world| {
+            start_anvil(fork_url.clone(), chain_id.clone(), world).boxed_local()
+        })
+    /*
+.with_writer(
+    // Output to both console and JUnit XML
+    // NOTE: `Writer`s pipeline is constructed in a reversed order.
+    writer::Basic::stdout() // And output to STDOUT.
+        .summarized()       // Simultaneously, add execution summary.
+        .tee::<SCWorld, _>(writer::JUnit::for_tee(file, 0)) // Then, output to XML file.
+        .normalized()       // First, normalize events order.
+        )
+     */
+        .run("tests/features")
+        .await;
+}
+
